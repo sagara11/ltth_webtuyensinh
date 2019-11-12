951c4ac471ad55da6bf326869a18f462c211524c
Tran Hung Phong <cityzen0910@gmail.com>
2019-11-12 23:13:04 +0700

    hoan thien them giao dien web trang chu, chua responsive

--- resources/views/user/layout/header.blade.php
+++ resources/views/user/layout/header.blade.php
@@ -1,39 +1,85 @@
 <!-- Page header -->
-<header class="mr-5 ml-5">
-    <div class="row p-1">
-        <div class="col-lg-9">
-            <p>
-                <i class="fa fa-edit"></i>
-                <b>SỰ KIỆN: </b>"Tuyển sụng 50 nhân viên thì có tới 49 sinh viên
-                trường nghề trúng tuyển" | "Tuyển dụng....."
-            </p>
-        </div>
-        <div class="dropdown d-flex col-lg-3">
-            <div class="d-flex dropdown-toggle" data-toggle="dropdown">
-                <img height="50px" width="50px" src="http://localhost\baotuyensinhView\media\tải xuống (1).png"
-                    alt="" />
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="header-news col-lg-9">
                 <p>
-                    Nguyễn Văn Nam
+                    <i class="fa fa-edit"></i>
+                    <b>SỰ KIỆN: </b>"Tuyển sụng 50 nhân viên thì có tới 49 sinh viên
+                    trường nghề trúng tuyển" | "Tuyển dụng....."
                 </p>
             </div>
-            <div class="dropdown-menu">
-                menu
+            <div class="header-account dropdown col-lg-3">
+                <div class="dropdown-toggle" data-toggle="dropdown">
+                    <img class=" rounded rounded-circle" height="25px" width="25px" src="http://localhost\baotuyensinhView\media\tải xuống (1).png"
+                        alt="" />
+                    <p>
+                        Nguyễn Văn Nam
+                    </p>
+                </div>
+                <div class="dropdown-menu">
+                    <ul class="list-unstyled">
+                        <li data-toggle="modal" data-target="#signin">
+                            Đăng nhập
+                        </li>
+                        <li data-toggle="modal" data-target="#signup">
+                            Đăng ký
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
 </header>
 
+<!-- Dang nhap, dang ky -->
+<article class="modal fade" id="signin">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header" data-dismiss="modal">
+                <button class="btn btn-danger">
+                    Close
+                </button>
+            </div>
+            dang nhap
+            <div class="modal-footer">
+                <button class="btn btn-success">
+                    Đăng nhập
+                </button>
+            </div>
+        </div>
+    </div>
+</article>
+
+<article class="modal fade" id="signup">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header" data-dismiss="modal">
+                <button class="btn btn-danger">
+                    Close
+                </button>
+            </div>
+            dang ky
+            <div class="modal-footer">
+                <button class="btn btn-success">
+                    Tạo tài khoản
+                </button>
+            </div>
+        </div>
+    </div>
+</article>
+
 <!-- Navigation -->
-<nav class="container bg-light">
-    <div class="row">
+<nav class="container">
+    <div class="nav row">
         <a href="" class="col-lg-2 nav-logo">
             <img class="img-fluid" src="http://localhost\baotuyensinhView\media\1 Trang chủ.png" alt="" />
         </a>
-        <a href="" class="col-lg-1">
+        <a class="home" href="" class="col-lg-1">
             <i class="fa fa-home"></i>
         </a>
-        <div class="col-lg-8">
-            <ul class="row list-unstyled">
+        <div class="menu col-lg-8">
+            <ul class="row">
                 <li class="col-lg-2">
                     <a href=""><b>GIÁO DỤC</b></a>
                 </li>
