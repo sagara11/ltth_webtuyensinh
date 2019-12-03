f100ee4fc66a1fa89318f4968f326b0e53402d73
Minh Thong <thiensangkdtt@gmail.com>
2019-12-03 23:18:06 +0700

    Them file

---
+++ app/Http/Middleware/CheckLogin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Auth;
+
+use Closure;
+
+class CheckLogin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        if(Auth::check())
+        {
+            if(Auth::user()->role_id == 1)
+            {
+                return redirect('/home');
+            }
+            else
+            {
+                Auth::logout();
+                return redirect('/login');
+            }
+        }
+        return $response;
+    }
+}
