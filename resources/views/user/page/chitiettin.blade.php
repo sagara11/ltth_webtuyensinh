498883554f9b7b6326eed345098bb952337a2768
Tiến Đào <tiendao.online@gmail.com>
2019-12-06 20:15:25 +0700

    update code

--- resources/views/user/page/chitiettin.blade.php
+++ resources/views/user/page/chitiettin.blade.php
@@ -13,7 +13,7 @@
 
 <meta property="og:locale" content="en_US" />
 <meta property="og:type" content="website" />
-<meta property="og:title" content="{{ $new->description }}" />
+<meta property="og:title" content="{{ $new->name }}" />
 <meta property="og:url" content="https://webtuyensinh.edu.vn/{{$new->slug}}" />
 <meta property="og:site_name" content="{{ $new->name }}" />
 <meta property="og:description" content="{{ $new->description }}" />
